56ec816064c610b322a6534d882d4d054784e253
dlflowers12 <dl.flowers@icloud.com>
2026-05-25 23:03:17 -0400

    Update LeShea Group branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "LeShea Group";
+
+export const SITE_DESCRIPTION =
+  "LeShea Group is a purpose-driven brand house focused on education, leadership, media, workshops, community impact, and transformational experiences.";
